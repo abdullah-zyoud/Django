cfba03b40c33feffaaad33c0e3b36280d8106424
Abdullah Zyoud <aboodzyoud.1997@gmail.com>
2023-05-08 19:30:36 +0300

    edit forloop

--- For Loops Basic I/python.py
+++ For Loops Basic I/python.py
@@ -1,19 +1,24 @@
-# for x in range(150):
+# for x in range(1,151):
 #     print (x)
 
 # for x in range(5,1000):
-#      print(x*5)
+#      if x%5==0:
+#         print(x)
 # for x in range(1,100):
-#        if x%5==0:
+#     if x%10==0 :
 #         print("coding")
-#        else :
+#     elif x%5==0 :
 #         print("coding dojo")
+#     else :
+#         print(x)
 # sum=0
-# for x in range(500000):
+# for x in range(500000+1):
       
-#        if x%2!=0:
+#     if x%2!=0:
 #         sum+=x
-# print (sum)    
+# print (sum)   
+
+
 # for x in range(2018,0,-4):
 #       print(x)
 # lowNum=2
@@ -21,19 +26,18 @@
 # mult=3
 # for x in range(lowNum,highNum+1):
 #       if x%mult==0:
-#             print(x)#1
+#             print(x)
 
-#15
-def a():
-    print(1)
-    x = b()
-    print(x)
-    return 10
-def b():
-    print(3)
-    return 5
-y = a()
-print(y)
+# def a():
+#     print(1)
+#     x = b()
+#     print(x)
+#     return 10
+# def b():
+#     print(3)
+#     return 5
+# y = a()
+# print(y)
 
 
 
