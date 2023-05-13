bab438303e124597805724ad459f0abca335b9a8
Abdullah Zyoud <aboodzyoud.1997@gmail.com>
2023-05-13 11:50:28 +0300

    chaning method

--- _python/python_fundamentals/User Objectives.py
+++ _python/python_fundamentals/User Objectives.py
@@ -1,22 +1,23 @@
 class User:		
-    def __init__(self, name, email):
+     def __init__(self, name, email):
         self.name = name
         self.email = email
         self.account_balance = 0
-    def make_deposit(self, amount):	
-    	self.account_balance += amount	
-    def make_withdrawal(self, amount):
-        self.account_balance -= amount	
-    def display_user_balance(self) :
-        print(f"user name :{self.name} , account balanse :{self.account_balance}")   
-    def transfer_money(self, other_user, amount):
         
-        pass
+    
+     def make_deposit(self, amount):	
+        self.account_balance += amount	
+        return self
+    
+     def make_withdrawal(self, amount):
+        self.account_balance -= amount	
+        return self
+    
+     def display_user_balance(self) :
+        print(f"user name :{self.name} , account balanse :{self.account_balance}") 
+        return self  
+    
 
 user_1=User("ahmad","ahmad.com")
-user_1.make_deposit(1000)
-print(user_1.account_balance)
-user_1.make_withdrawal(500)
-print(user_1.name)
-print(user_1.account_balance)
-user_1.display_user_balance()
+user_1.make_deposit(100).make_deposit(200).make_deposit(300).make_withdrawal(50).display_user_balance()
+
